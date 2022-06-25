1b55cfa02507ebdd98c439cd545d49dc8102c3c8
Gary DESHAYES <deshayesgary@hotmail.fr>
2022-06-25 14:24:59 +0200

    Ajouts de routes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,21 @@
-import './Header.scss';
+import './Header.scss'
+import { Link } from 'react-router-dom'
 
 function Header() {
   return (
     <div className="header">
-     <h1>Gary DESHAYES - Freelance Développeur Web Full-Stack</h1>
-     <h2>Arbre de compétences</h2>
+      <h1>Gary DESHAYES - Freelance Développeur Web Full-Stack</h1>
+      <nav
+        style={{
+          borderBottom: 'solid 1px',
+          paddingBottom: '1rem',
+        }}
+      >
+        <Link to="/experiences">Experience</Link>
+      </nav>
+      <h2>Arbre de compétences</h2>
     </div>
-  );
+  )
 }
 
-export default Header;
+export default Header
---
+++ src/routes/experience.jsx
@@ -0,0 +1,11 @@
+
+
+function Experience() {
+  return (
+    <div className="experience">
+      <h1>Experience</h1>
+    </div>
+  );
+}
+
+export default Experience;
